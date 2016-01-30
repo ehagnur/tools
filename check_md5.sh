ef438ae85b00d93ca3eed72106ea86451b53c025
Hagos Salih <hsalih@box.com>
2016-01-30 14:07:31 -0800

    deleting unwated script

--- check_md5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for status in $(md5sum -c md5sums.md5 |awk '{print $NF}') 
-	do 
-		if [[ $status == "OK" ]]; then 
-			echo "Certificate is the same" 
-		else 
-			echo "Certificate has changed" 
-		fi 
-	done
